3d524d1ba2af8669f4aa8e7459c84c0c983b76c7
ariespirnando <aries.pirnando@gmail.com>
2024-07-02 22:01:54 +0700

    next monitoring

---
+++ app/view-all/page.tsx
@@ -0,0 +1,23 @@
+import { Card } from '@/components/ui/card'
+import React from 'react'
+
+const viewAll = () => {
+  return (
+    <div className="grid gap-4">
+         <Card
+            className="xl:col-span-2" x-chunk="dashboard-01-chunk-4"
+          >
+
+
+
+
+
+
+
+            
+          </Card>
+        </div>
+  )
+}
+
+export default viewAll
---
+++ components/dashboard.tsx
@@ -0,0 +1,89 @@
+import Link from "next/link"
+import {
+  Activity,
+  ArrowUpRight,
+} from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Table
+} from "@/components/ui/table"
+
+export function Dashboard() {
+  return (
+    <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
+          <Card
+            className="xl:col-span-2" x-chunk="dashboard-01-chunk-4"
+          >
+            <CardHeader className="flex flex-row items-center">
+              <div className="grid gap-2"> 
+                <CardDescription>
+                  Recent Error transactions from CBS
+                </CardDescription>
+              </div>
+              <Button asChild size="sm" className="ml-auto gap-1">
+                <Link href="#">
+                  View All
+                  <ArrowUpRight className="h-4 w-4" />
+                </Link>
+              </Button>
+            </CardHeader>
+            <CardContent>
+              <Table>
+                
+              </Table>
+            </CardContent>
+          </Card>
+
+          <Card x-chunk="dashboard-01-chunk-5">
+            <CardHeader>
+              <CardTitle>Recent Transaction</CardTitle>
+            </CardHeader>
+            <CardContent className="grid gap-8">
+                <Card x-chunk="dashboard-01-chunk-3">
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">Completed</CardTitle>
+                    <Activity className="h-4 w-4 text-muted-foreground" />
+                    </CardHeader>
+                    <CardContent>
+                    <div className="text-2xl font-bold">+573</div>
+                    <p className="text-xs text-muted-foreground">
+                        +201 since last hour
+                    </p>
+                    </CardContent>
+                </Card>
+                <Card x-chunk="dashboard-01-chunk-3">
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">Processing</CardTitle>
+                    <Activity className="h-4 w-4 text-muted-foreground" />
+                    </CardHeader>
+                    <CardContent>
+                    <div className="text-2xl font-bold">+573</div>
+                    <p className="text-xs text-muted-foreground">
+                        +201 since last hour
+                    </p>
+                    </CardContent>
+                </Card>
+                <Card x-chunk="dashboard-01-chunk-3">
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">Error</CardTitle>
+                    <Activity className="h-4 w-4 text-muted-foreground" />
+                    </CardHeader>
+                    <CardContent>
+                    <div className="text-2xl font-bold">+573</div>
+                    <p className="text-xs text-muted-foreground">
+                        +201 since last hour
+                    </p>
+                    </CardContent>
+                </Card>
+            </CardContent>
+          </Card>
+        </div>
+  )
+}
